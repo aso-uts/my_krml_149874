e2a8afc2801043fbfaea6d3ebd10e20189e42e2e
Anthony So <anthony.so@uts.edu.au>
2023-07-14 21:47:33 +1000

    add assess regressor

--- src/my_krml_149874/models/performance.py
+++ src/my_krml_149874/models/performance.py
@@ -1,4 +1,4 @@
-def print_reg_perf(y_preds, y_actuals, set_name=None):
+def print_regressor_scores(y_preds, y_actuals, set_name=''):
     """Print the RMSE and MAE for the provided data
 
     Parameters
@@ -18,3 +18,48 @@ def print_reg_perf(y_preds, y_actuals, set_name=None):
 
     print(f"RMSE {set_name}: {mse(y_actuals, y_preds, squared=False)}")
     print(f"MAE {set_name}: {mae(y_actuals, y_preds)}")
+
+
+def assess_regressor_set(model, features, target, set_name=''):
+    """Save the predictions from a trained model on a given set and print its RMSE and MAE scores
+
+    Parameters
+    ----------
+    model: sklearn.base.BaseEstimator
+        Trained Sklearn model with set hyperparameters
+    features : Numpy Array
+        Features
+    target : Numpy Array
+        Target variable
+    set_name : str
+        Name of the set to be printed
+
+    Returns
+    -------
+    """
+    preds = model.predict(features)
+    print_regressor_scores(y_preds=preds, y_actuals=target, set_name=set_name)
+
+
+def fit_assess_regressor(model, X_train, y_train, X_val, y_val):
+    """Train a regressor model and print its RMSE and MAE scores on the training and validation set
+
+    Parameters
+    ----------
+    model: sklearn.base.BaseEstimator
+        Instantiated Sklearn model with set hyperparameters
+    X_train : Numpy Array
+        Features for the training set
+    y_train : Numpy Array
+        Target for the training set
+    X_train : Numpy Array
+        Features for the validation set
+    y_train : Numpy Array
+        Target for the validation set
+
+    Returns
+    -------
+    """
+    model.fit(X_train, y_train)
+    assess_regressor_set(model, X_train, y_train, set_name='Training')
+    assess_regressor_set(model, X_val, y_val, set_name='Validation')
